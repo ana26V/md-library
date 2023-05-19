3b07ba17abd95ca27017fac42332622dfe1ac69c
ana26V <anamariavoichescu26@gmail.com>
2023-05-19 17:44:12 +0300

    upd

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -48,7 +48,7 @@ export default function SearchBar() {
           type="search"
           variant="standard"
           sx={{ width: 300 }}
-          onChange={handleChange}
+        
         />
     
     </>
